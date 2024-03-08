eb8afefbc5deb6f763d8f3f24be12ae6f7dbb351
HOchacha <outcider112@dankook.ac.kr>
2024-03-08 06:54:17 +0000

    initial commit

---
+++ client/client.py
@@ -0,0 +1,15 @@
+import socket
+import time
+
+def start_client():
+    while True:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('server', 12345))
+        client_socket.sendall(b'Hello world')
+        data = client_socket.recv(1024)
+        print(f"Received {data} from server")
+        client_socket.close()
+        time.sleep(2)
+
+if __name__ == "__main__":
+    start_client()
